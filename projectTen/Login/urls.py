7bffd115bce77b9b6071fa222589e1a89f83b6e4
CHE <1095735146@qq.com>
2020-04-06 00:31:17 +0800

    CheShiqi,the second time,login and logout and register

--- projectTen/Login/urls.py
+++ projectTen/Login/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'Login'
 urlpatterns = [
-    path('', views.page, name = 'page'),
-    path('login/', views.login, name = 'login'),
+    path('', views.主页, name = '主页'),
+    path('login/', views.登录, name = '登录'),
+    path('logout/', views.登出, name = '登出'),
+    path('register/', views.注册, name = '注册'),
 ]
